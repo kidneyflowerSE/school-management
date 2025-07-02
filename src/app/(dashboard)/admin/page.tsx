481d50667594fdd897a63f80b18b90d5388e82f3
kidneyflowerSE <ilovehtmlandcss@gmail.com>
2025-07-02 12:15:44 +0700

    test PR

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const AdminPage = () => {
   return (
-    <div className=''>Page</div>
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* Left */}
+      <div className='w-full lg:w-2/3'>
+
+      </div>
+      {/* Right */}
+      <div className='w-full lg:w-1/3'>
+
+      </div>
+    </div>
   )
 }
 
