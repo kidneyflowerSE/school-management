edc89b66c3faeb279504bbed9e01d606e75cfd36
kidneyflowerSE <ilovehtmlandcss@gmail.com>
2025-07-02 11:56:33 +0700

    chore: add sidebar UI

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div className=''>Page</div>
+  )
+}
+
+export default AdminPage
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>SignInPage</div>
+  )
+}
+
+export default LoginPage
