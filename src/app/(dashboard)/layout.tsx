4a0d8deec6961b94aaf33ed58ceaa4843f65fffd
kidneyflowerSE <ilovehtmlandcss@gmail.com>
2025-07-02 16:54:43 +0700

    chore: add UI for admin

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import Menu from "../components/Menu";
-import Navbar from "../components/Navbar";
+import Menu from "../../components/Menu";
+import Navbar from "../../components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -10,10 +10,10 @@ export default function DashboardLayout({
 }>) {
   return <div className="h-screen flex">
     {/* Left Sidebar */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-2">
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
       <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
       <Image src="/logo.png" alt="logo" width={32} height={32} />
-      <span className="hidden lg:block">School Management</span>
+      <span className="hidden lg:block">School Yaohui</span>
       </Link>
       <Menu />
     </div>
--- src/app/components/UserCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UserCard = ({type}: {type: string}) => {
-  return (
-    <div>UserCard</div>
-  )
-}
-
-export default UserCard
